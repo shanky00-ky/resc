66463e54fd9d360aae8ea800e2fcb687f9ae5b5a
shanky00-ky <shankarbhai75180@gmail.com>
2024-11-18 10:01:22 +0530

    Update script.js

--- content/scripts/script.js
+++ content/scripts/script.js
@@ -111,3 +111,35 @@ const checkbox = document.getElementById('checkbox');
 checkbox.addEventListener('change', () => {
     document.body.classList.toggle('dark-mode', checkbox.checked);
 });
+
+jQuery("#carousel").owlCarousel({
+  autoplay: true,
+  rewind: true, /* use rewind if you don't want loop */
+  margin: 20,
+   /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 3
+    },
+
+    1024: {
+      items: 4
+    },
+
+    1366: {
+      items: 4
+    }
+  }
+});
